2c9e71bfb0d93206ae58e499708475b8758b165d
DaniloJendick <42503051+DaniloJendick@users.noreply.github.com>
2022-06-02 13:56:27 +0100

    Add files via upload

---
+++ Last Day Final Product/services/signin.js
@@ -0,0 +1,25 @@
+import { createSession, findUserByEmail } from "../data/signupin.js"
+
+export async function login(email, password, res) {
+    const user = await findUserByEmail(email)
+    console.log(user)
+    //verificar se o email existe
+    if (!user) {
+        //se não, 404 e { "message": "O utilizador não foi encontrado!" }
+        return res.status(404).json({ message: "O utilizador não foi encontrado!" })
+    }
+    console.log("usuario verificado")
+    console.log(password)
+    //se a password é igual
+    if (user.password !== password) {
+        return res.status(401).json({ message: "A password introduzida é inválida!" })
+    }
+    console.log("senha verificado")
+    const token = await createSession({ userId: user._id })
+    console.log("token")
+    console.log(token)
+    console.log("userId")
+    console.log(user._id)
+    console.log({ token: token.insertedId, userId: user._id })
+    return res.status(200).json({ token: token.insertedId, userId: user._id })
+}
